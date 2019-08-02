73a0e07cd9aa2708e7ba376088eb9395c988391b
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-02 01:44:49 -0400

    added sections @ src/views/HomeView/index.js

--- src/views/HomeView/index.js
+++ src/views/HomeView/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import NavbarSection from '../../components/NavbarSection'
+import IntroSection from '../../components/IntroSection'
+import AgentSection from '../../components/AgentSection'
+import PricingSection from '../../components/PricingSection'
+import LaunchSection from '../../components/LaunchSection'
+import MonitorSection from '../../components/MonitorSection'
+import PaySection from '../../components/PaySection'
+import SignupSection from '../../components/SignupSection'
+import FooterSection from '../../components/FooterSection'
 
 const HomeView = () => (
   <Layout>
     <section id="navbar" className="bg-primary"></section>
-    <section id="intro" className="bg-primary"></section>
-    <section id="market"></section>
-    <section id="pricing" className="bg-light"></section>
-    <section id="launch"></section>
-    <section id="monitor" className="bg-primary-light"></section>
-    <section id="pay"></section>
-    <section id="signup" className="bg-primary"></section>
-    <section id="footer" className="bg-dark"></section>
+    <NavbarSection className="bg-primary" />
+    <IntroSection className="bg-primary" />
+    <AgentSection />
+    <PricingSection className="bg-light" />
+    <LaunchSection />
+    <MonitorSection className="bg-primary-light" />
+    <PaySection />
+    <SignupSection className="bg-primary" />
+    <FooterSection className="bg-dark" />
   </Layout>
 );
 
