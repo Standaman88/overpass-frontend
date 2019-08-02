8354ffb71725468c00d63f87ae485030e3bf11fd
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-02 01:52:47 -0400

    refactor src/components/AgentSection

--- src/components/AgentSection/index.js
+++ src/components/AgentSection/index.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components'
 import Section from '../Section'
+import { AgentSectionWrap } from './styles'
 
-const AgentSection = styled(Section)`
 
-`;
+const AgentSection = () => (
+  <AgentSectionWrap>
+    Agent Section
+  </AgentSectionWrap>
+);
 
 export default AgentSection;
---
+++ src/components/AgentSection/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import Section from '../Section'
+
+export const AgentSectionWrap = styled(Section)`
+  // styles go here
+`;
