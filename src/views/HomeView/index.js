85ea7a09112a57b24dbb84636c73b20c1ff612d0
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-02 01:20:22 -0400

    added src/views/HomeView/index.js

---
+++ src/views/HomeView/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const HomeView = () => (
+  <Layout>
+    <section id="navbar" className="bg-primary"></section>
+    <section id="intro" className="bg-primary"></section>
+    <section id="market"></section>
+    <section id="pricing" className="bg-light"></section>
+    <section id="launch"></section>
+    <section id="monitor" className="bg-primary-light"></section>
+    <section id="pay"></section>
+    <section id="signup" className="bg-primary"></section>
+    <section id="footer" className="bg-dark"></section>
+  </Layout>
+);
+
+export default HomeView;
