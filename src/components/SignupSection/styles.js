6e5fc06dc1141f253d979b0e17bc34c1cff8e633
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-02 03:08:42 -0400

    added bg @ src/components/SignupSection

--- src/components/SignupSection/styles.js
+++ src/components/SignupSection/styles.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import { SectionPrimary } from '../Section'
+import SignupImage from './images/footer.svg'
+
 
 export const SignupSectionWrap = styled(SectionPrimary)`
-  // styles go here
+  background-image: url(${SignupImage});
+  background-position: center right;
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 550px;
 `;
