f623b52dbbbe377716882be286d3c741cf77bd1b
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-02 03:46:21 -0400

    enhance src/components/InfoBlock

--- src/components/InfoBlock/index.js
+++ src/components/InfoBlock/index.js
@@ -3,18 +3,20 @@ import styled from 'styled-components'
 import Icon from '../Icon'
 import Section from '../Section'
 import { Column, Row } from '../Layout'
-import { InfoBlockWrap } from './styles'
+import { InfoBlockWrap, Title, Description } from './styles'
 
 
-const InfoBlock = ({ icon, title, description, children }) => (
+const InfoBlock = ({ icon="analytics", title, description, children }) => (
   <InfoBlockWrap>
     <Row>
       <Column>
-        <Icon name={icon}} />
+        <Icon name={icon} />
       </Column>
       <Column>
-        <Title>{title}</Title>
-        <Description>{ description || children }</Description>
+        <div>
+          <Title>{title}</Title>
+          <Description>{ description || children }</Description>
+        </div>
       </Column>
     </Row>
   </InfoBlockWrap>
--- src/components/InfoBlock/styles.js
+++ src/components/InfoBlock/styles.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 import { SectionPrimary } from '../Section'
-import InfoBlockImage from './images/hero.svg'
 
 
 export const InfoBlockWrap = styled.div`
-
+  display: inline-flex;
+  max-width: 300px;
 `;
 
 export const Title = styled.h4`
   font-size: 20px;
 `;
 
-export const Descripton = styled.p`
+export const Description = styled.p`
   font-size: 14px;
 `;
