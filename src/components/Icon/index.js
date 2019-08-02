bf9b5c06a0e0e71941f124d7fa3c011ce9afe18a
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-02 03:17:22 -0400

    added src/components/Icon

---
+++ src/components/Icon/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+import IconAnalytics from '/images/analytics.svg'
+import IconBilling from '/images/billing.svg'
+import IconCalling from '/images/calling.svg'
+import IconInternational from '/images/international.svg'
+import IconOutcomes from '/images/outcomes.svg'
+import IconPayments from '/images/payments.svg'
+import IconRecording from '/images/recording.svg'
+import IconScript from '/images/script.svg'
+import IconUpload from '/images/upload.svg'
+
+const ICONS = {
+  analytics: IconAnalytics,
+  billing: IconBilling,
+  calling: IconCalling,
+  international: IconInternational,
+  outcomes: IconOutcomes,
+  payments: IconPayments,
+  recording: IconRecording,
+  script: IconScript,
+  upload: IconUpload,
+};
+
+export const Icon = styled.img(({ src="payments" }) => ({
+  src: src in ICONS ? ICONS[src] : src
+}))`
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  height: 30px;
+  width: 30px;
+`;
+
+export default Icon;
