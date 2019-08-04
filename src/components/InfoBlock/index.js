7b5c921282848bb174b1c6092e487d6b9e7cda6f
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 19:27:37 -0400

    updated src/components/InfoBlock

--- src/components/InfoBlock/index.js
+++ src/components/InfoBlock/index.js
@@ -3,15 +3,17 @@ import styled from 'styled-components'
 import Icon from '../Icon'
 import Section from '../Section'
 import { Column, Row } from '../Layout'
-import { InfoBlockWrap, Title, Description } from './styles'
+import { InfoBlockWrap, InfoBlockIcon, Title, Description } from './styles'
+
+
 
 
 const InfoBlock = ({ icon="analytics", title, description, children }) => (
   <InfoBlockWrap>
     <Row>
-      <Column>
+      <InfoBlockIcon>
         <Icon name={icon} />
-      </Column>
+      </InfoBlockIcon>
       <Column>
         <div>
           <Title>{title}</Title>
--- src/components/InfoBlock/styles.js
+++ src/components/InfoBlock/styles.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column } from '../Layout'
 import { SectionPrimary } from '../Section'
 
 
@@ -8,10 +9,23 @@ export const InfoBlockWrap = styled.div`
   max-width: 300px;
 `;
 
+export const InfoBlockIcon = styled(Column)`
+  justify-content: flex-start;
+  margin-right: 20px;
+`;
+
 export const Title = styled.h4`
+  color: #1E95EE;
+  font-family: Arial, san-serif;
   font-size: 20px;
+  font-weigth: bold;
+  margin: 0;
+  margin-bottom: 0.5em;
 `;
 
 export const Description = styled.p`
+  color: #1E95EE;
+  font-family: Arial, san-serif;
   font-size: 14px;
+  line-height: 1.25em;
 `;
