228f648cdad1760c1bcd5a7f99c9e85c19e55002
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 19:43:25 -0400

    updated InfoBlock text @ src/components/MonitorSection/index.js

--- src/components/MonitorSection/index.js
+++ src/components/MonitorSection/index.js
@@ -10,9 +10,9 @@ const MonitorSection = () => (
   <MonitorSectionWrap>
     Monitor Section
     <Row>
-      <InfoBlock icon="analytics" title="Test Title">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBlock>
-      <InfoBlock icon="analytics" title="Test Title">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBlock>
-      <InfoBlock icon="analytics" title="Test Title">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="analytics" title="Call Outcomes">Easily view the outcomes of all calls and quickly evaulate your campaign success.</InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="analytics" title="Recording">Calls are automatically recorded and saved, and can be listened to at any time.</InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="analytics" title="Analytics">Gain insights into the performance and success of calls and campaigns.</InfoBlock>
     </Row>
   </MonitorSectionWrap>
 );
