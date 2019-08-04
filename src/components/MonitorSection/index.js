c23ae45651d3f530c1b964c00d891200b929840d
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 19:48:57 -0400

    updated InfoBlock text @ src/components/MonitorSection/index.js

--- src/components/MonitorSection/index.js
+++ src/components/MonitorSection/index.js
@@ -10,8 +10,8 @@ const MonitorSection = () => (
   <MonitorSectionWrap>
     Monitor Section
     <Row>
-      <InfoBlock color="#3A3C3E" icon="analytics" title="Call Outcomes">Easily view the outcomes of all calls and quickly evaulate your campaign success.</InfoBlock>
-      <InfoBlock color="#3A3C3E" icon="analytics" title="Recording">Calls are automatically recorded and saved, and can be listened to at any time.</InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="outcomes" title="Call Outcomes">Easily view the outcomes of all calls and quickly evaulate your campaign success.</InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="recording" title="Recording">Calls are automatically recorded and saved, and can be listened to at any time.</InfoBlock>
       <InfoBlock color="#3A3C3E" icon="analytics" title="Analytics">Gain insights into the performance and success of calls and campaigns.</InfoBlock>
     </Row>
   </MonitorSectionWrap>
