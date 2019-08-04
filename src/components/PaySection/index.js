f1e7a8743568f7d52ed4469a5e31134d9eaf5964
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 19:57:00 -0400

    updated InfoBlocks @ src/components/PaySection/index.js

--- src/components/PaySection/index.js
+++ src/components/PaySection/index.js
@@ -10,9 +10,16 @@ const PaySection = () => (
   <PaySectionWrap>
     Pay Section
     <Row>
-      <InfoBlock icon="analytics" title="Test Title">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBlock>
-      <InfoBlock icon="analytics" title="Test Title">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBlock>
-      <InfoBlock icon="analytics" title="Test Title">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="payments" title="Status-based Payments">
+        Pay only for the time your reps are actively working on the system–nothing more.
+      </InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="international" title="Pay Internationally">
+        With reps from all over the globe, payments can get tricky.
+        We handle international payments so you don’t have to.
+      </InfoBlock>
+      <InfoBlock color="#3A3C3E" icon="billing" title="Integrated Billing">
+        We facilitate payments on your behalf and provide you with comprehensive invoices.
+      </InfoBlock>
     </Row>
   </PaySectionWrap>
 );
