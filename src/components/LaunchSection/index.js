16147601beda2b7c00169722b9841ff7ede1aba6
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 20:51:44 -0400

    added HighlightBox and Image to @ src/components/LaunchSection/

--- src/components/LaunchSection/index.js
+++ src/components/LaunchSection/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import HighlightBox from '../HighlightBox'
 import InfoBox from '../InfoBox'
 import { Column, Row } from '../Layout'
 import Section from '../Section'
 import { LaunchSectionWrap } from './styles'
+import LaunchImage from './images/launch.svg'
 
 
 const LaunchSection = () => (
   <LaunchSectionWrap>
-    Launch Section
+    <Row>
+      <HighlightBox title="Launch" position="left">
+        Set up your first campaign and get your reps right to work
+        on our fully integrated, browser-based platform.
+      </HighlightBox>
+      <Column>
+        <img src={LaunchImage} />
+      </Column>
+    </Row>
     <Row>
       <InfoBox icon="upload" title="Upload your leads">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBox>
       <InfoBox icon="calling" title="Start Calling">Use custom outbound numbers, calling queues and our preview dialer to ensure success with your call campaigns.</InfoBox>
