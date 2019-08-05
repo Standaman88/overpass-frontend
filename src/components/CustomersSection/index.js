308d4745d41a6ba646d08b90ce60d1698573a8fc
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 02:23:58 -0400

    done src/components/CustomersSection/index.js

--- src/components/CustomersSection/index.js
+++ src/components/CustomersSection/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column, Row, ResponsiveRow } from '../Layout'
+import Container from '../Container'
+import Logo from '../Logo'
 import Section from '../Section'
+import Title from '../Title'
 import { CustomersSectionWrap } from './styles'
 
 
 const CustomersSection = () => (
   <CustomersSectionWrap>
-    Customers Section
+    <Container>
+      <Title as="h6" color="white" style={{fontWeight:'normal',marginBottom:25,textAlign:'center'}}>
+        TRUSTED BY
+      </Title>
+      <Row>
+        <Logo name="monday" />
+        <Logo name="intel" />
+        <Logo name="johnson" />
+        <Logo name="handy" />
+        <Logo name="flexport" />
+      </Row>
+    </Container>
   </CustomersSectionWrap>
 );
 
