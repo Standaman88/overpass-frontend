c3f4446f7266270e6956d264ae1540987162d2a5
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 03:02:10 -0400

    done src/components/HeroSection/index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column, Row, ResponsiveRow, Half } from '../Layout'
+import Container from '../Container'
+import EmailForm from '../EmailForm'
+import Paragraph from '../Paragraph'
 import Section from '../Section'
+import Title from '../Title'
 import { HeroSectionWrap } from './styles'
+import HeroImage from './images/hero.svg'
 
 
 const HeroSection = () => (
   <HeroSectionWrap>
-    Hero Section
+    <Container>
+      <ResponsiveRow>
+        <Half>
+          <Title as="h3" color="white">
+            Find, hire & manage your own remote sales team
+          </Title>
+          <Paragraph color="white">
+            Overpass provides the talent and technology to
+            make all of your sales campaigns a success
+          </Paragraph>
+          <EmailForm />
+          <Paragraph color="white" size="small">
+            Get started in minutes. No credit card required.
+          </Paragraph>
+        </Half>
+        <Half></Half>
+      </ResponsiveRow>
+    </Container>
   </HeroSectionWrap>
 );
 
