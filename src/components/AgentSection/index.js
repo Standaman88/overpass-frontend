f97e1b7b42654ed0dcb76bd5f27c5a5950dc67ed
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 01:20:07 -0400

    added Container @ src/components/AgentSection/index.js

--- src/components/AgentSection/index.js
+++ src/components/AgentSection/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Button from '../Button'
+import Container from '../Container'
 import { Column, Row, ResponsiveRow } from '../Layout'
 import Paragraph from '../Paragraph'
 import Section from '../Section'
@@ -12,24 +13,26 @@ import ProfilesImage from './images/profiles.svg'
 
 const AgentSection = () => (
   <AgentSectionWrap>
-    <ResponsiveRow>
-      <Half>
-        <img src={SearchImage} style={{padding:20}} />
-        <Title as="h3">Find the perfect agent</Title>
-        <Paragraph color="#3A3C3E" size="large" style={{maxWidth:445,textAlign:'center'}}>
-          No more browsing through thousands of applications.
-          Each week, we’ll send you a fresh batch of hand-picked,
-          personally-vetted candidates.
-        </Paragraph>
-        <Button>Try today</Button>
-      </Half>
-      <Half>
-        <Subtitle as="h5">
-          Selections from the <br />Overpass Marketplace
-        </Subtitle>
-        <img src={ProfilesImage} />
-      </Half>
-    </ResponsiveRow>
+    <Container>
+      <ResponsiveRow>
+        <Half>
+          <img src={SearchImage} style={{padding:20}} />
+          <Title as="h3">Find the perfect agent</Title>
+          <Paragraph color="#3A3C3E" size="large" style={{maxWidth:445,textAlign:'center'}}>
+            No more browsing through thousands of applications.
+            Each week, we’ll send you a fresh batch of hand-picked,
+            personally-vetted candidates.
+          </Paragraph>
+          <Button>Try today</Button>
+        </Half>
+        <Half>
+          <Subtitle as="h5">
+            Selections from the <br />Overpass Marketplace
+          </Subtitle>
+          <img src={ProfilesImage} />
+        </Half>
+      </ResponsiveRow>
+    </Container>
   </AgentSectionWrap>
 );
 
