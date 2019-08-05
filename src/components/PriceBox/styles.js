04003ab437691487ad6071c9bf11ff6ec2e45244
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 23:06:44 -0400

    added src/components/PriceBox

---
+++ src/components/PriceBox/styles.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Column } from '../Layout'
+import { SectionPrimary } from '../Section'
+
+
+export const PriceBoxWrap = styled(Column)`
+  background: #FFFFFF;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px 0px rgba(0,0,0,.16);
+  display: inline-flex;
+  height: 100%;
+  max-width: 500px;
+  max-height: 240px;
+  width: 500px;
+
+  margin: 0 20px;
+  padding: 50px 0;
+
+  && * {
+    color: ${props => props.color || '#1E95EE'};
+  }
+`;
