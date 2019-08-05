682d3d0f93d740a3ceb9730e736fd9bf1c3f7d03
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 01:20:14 -0400

    added Container @ src/components/LaunchSection/index.js

--- src/components/LaunchSection/index.js
+++ src/components/LaunchSection/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Container from '../Container'
 import HighlightBox from '../HighlightBox'
 import InfoBox from '../InfoBox'
 import { Column, Row, ResponsiveRow } from '../Layout'
@@ -10,20 +11,22 @@ import LaunchImage from './images/launch.svg'
 
 const LaunchSection = () => (
   <LaunchSectionWrap>
-    <ResponsiveRow>
-      <HighlightBox title="Launch" position="left">
-        Set up your first campaign and get your reps right to work
-        on our fully integrated, browser-based platform.
-      </HighlightBox>
-      <Column>
-        <img src={LaunchImage} />
-      </Column>
-    </ResponsiveRow>
-    <Row style={{marginTop:50}}>
-      <InfoBox icon="upload" title="Upload your leads">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBox>
-      <InfoBox icon="calling" title="Start Calling">Use custom outbound numbers, calling queues and our preview dialer to ensure success with your call campaigns.</InfoBox>
-      <InfoBox icon="script" title="Write your script">Create personalized scripts to ensure brand consistency by specifying exactly what agents should say on the phone.</InfoBox>
-    </Row>
+    <Container>
+      <ResponsiveRow>
+        <HighlightBox title="Launch" position="left">
+          Set up your first campaign and get your reps right to work
+          on our fully integrated, browser-based platform.
+        </HighlightBox>
+        <Column>
+          <img src={LaunchImage} />
+        </Column>
+      </ResponsiveRow>
+      <Row style={{marginTop:50}}>
+        <InfoBox icon="upload" title="Upload your leads">Easily upload and manage your contacts with custom fields and dynamic lists.</InfoBox>
+        <InfoBox icon="calling" title="Start Calling">Use custom outbound numbers, calling queues and our preview dialer to ensure success with your call campaigns.</InfoBox>
+        <InfoBox icon="script" title="Write your script">Create personalized scripts to ensure brand consistency by specifying exactly what agents should say on the phone.</InfoBox>
+      </Row>
+    </Container>
   </LaunchSectionWrap>
 );
 
