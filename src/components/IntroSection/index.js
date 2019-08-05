ad2e7e629b9e0f2e5f621255cabd32a163405bd1
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 02:22:04 -0400

    removed CustomerSection + AboutSection @ src/components/IntroSection/index.js

--- src/components/IntroSection/index.js
+++ src/components/IntroSection/index.js
@@ -2,16 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 import Section from '../Section'
 import HeroSection from '../HeroSection'
-import CustomersSection from '../CustomersSection'
-import AboutSection from '../AboutSection'
 import { IntroSectionWrap } from './styles'
 
 
 const IntroSection = () => (
   <IntroSectionWrap>
     <HeroSection />
-    <CustomersSection />
-    <AboutSection />
   </IntroSectionWrap>
 );
 
