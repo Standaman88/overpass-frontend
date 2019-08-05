987a4af0a9195923f9c308e95a98f2bc5a05fcc8
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 03:16:53 -0400

    refactor src/components/EmailForm/index.js

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Row } from '../Layout'
+import Paragraph from '../Paragraph'
 import { EmailFormWrap, EmailField, SubmitButton } from './styles'
 
 
-export const EmailForm = ({ email }) => (
+
+export const EmailForm = ({ email, caption }) => (
   <EmailFormWrap>
     <Row>
       <EmailField type="email" placeholder="Enter your email" value={email} />
       <SubmitButton type="submit" value="Get started" />
+    </Row>
+    <Row>
+      {caption && <Paragraph color="white" size="small">{caption}</Paragraph>}
     </Row>
   </EmailFormWrap>
 );
