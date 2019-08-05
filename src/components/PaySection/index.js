7ecad0bc461ccf8a8004595040b0c0a15d355ce1
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 21:01:54 -0400

    added HighlightBox and Image @ src/components/PaySection

--- src/components/PaySection/index.js
+++ src/components/PaySection/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Column, Row } from '../Layout'
+import HighlightBox from '../HighlightBox'
 import InfoBox from '../InfoBox'
 import Section from '../Section'
 import { PaySectionWrap } from './styles'
+import PayImage from './images/pay.svg'
 
 
 const PaySection = () => (
   <PaySectionWrap>
-    Pay Section
     <Row>
+      <HighlightBox title="Pay" position="left">
+        No matter the size of your team or the number of features you use,
+        we make it easy to control your budget with clear, easy to understand billing.
+      </HighlightBox>
+      <Column>
+        <img src={PayImage} />
+      </Column>
+    </Row>
+    <Row style={{marginTop:50}}>
       <InfoBox color="#3A3C3E" icon="payments" title="Status-based Payments">
         Pay only for the time your reps are actively working on the system–nothing more.
       </InfoBox>
