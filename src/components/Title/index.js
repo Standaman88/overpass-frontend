710782452d5932de3cec49d2ffd11ea3ade2fcdb
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 21:33:21 -0400

    refactor src/components/Title/

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,11 +3,20 @@ import styled from 'styled-components'
 
 
 const Title = styled.h3`
+  color: #2A88FA;
   font-family: Arial, san-serif;
-  font-size: 42px;
+  /* font-size: 42px; */
   font-weigth: bold;
   margin: 0;
-  margin-bottom: 40px;
+  padding: 0;
+  /* margin-bottom: 40px; */
+
+  h1&& { font-size: 72px; }
+  h2&& { font-size: 42px; }
+  h3&& { font-size: 36px; }
+  h4&& { font-size: 20px; }
+  h5&& { font-size: 16px; }
+  h6&& { font-size: 12px; }
 `;
 
 export default Title;
