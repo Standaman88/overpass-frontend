b7960fcf597e1b3b7e12fa9bf324a96a87977ad5
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 22:38:34 -0400

    added Paragraph @ src/components/PricingSection/index.js

--- src/components/PricingSection/index.js
+++ src/components/PricingSection/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column } from '../Layout'
+import Paragraph from '../Paragraph'
 import Section from '../Section'
+import Title from '../Title'
 import { PricingSectionWrap } from './styles'
 
+const PricingParagraph = styled(Paragraph)`
+  line-height: 1.5em;
+  text-align: center;
+`;
 
 const PricingSection = () => (
   <PricingSectionWrap>
-    Pricing Section
+    <Column>
+      <Title as="h3" style={{marginBottom:20}}>Transparent Pricing</Title>
+      <PricingParagraph color="#3A3C3E" size="large" style={{maxWidth:650}}>
+        We’ve made our pricing as simple as possible.
+        With our free call center software, all you pay for is
+        talk-time and your contractor’s hourly rate.
+      </PricingParagraph>
+    </Column>
   </PricingSectionWrap>
 );
 
