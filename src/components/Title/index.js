c21e94618c3fc2e7e4f7107d5b2ecee05241fe01
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 21:17:13 -0400

    added src/components/Title/

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const Title = styled.h3`
+  font-family: Arial, san-serif;
+  font-size: 42px;
+  font-weigth: bold;
+  margin: 0;
+  margin-bottom: 40px;
+`;
