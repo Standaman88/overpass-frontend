1b4f017451c283d998e3a98a2a17cfc02c5578e9
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 01:26:37 -0400

    added Container @ src/components/PaySection/index.js

--- src/components/PaySection/index.js
+++ src/components/PaySection/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Column, Row, ResponsiveRow } from '../Layout'
+import Container from '../Container'
 import HighlightBox from '../HighlightBox'
 import InfoBox from '../InfoBox'
 import Section from '../Section'
@@ -10,27 +11,29 @@ import PayImage from './images/pay.svg'
 
 const PaySection = () => (
   <PaySectionWrap>
-    <ResponsiveRow>
-      <HighlightBox title="Pay" position="left">
-        No matter the size of your team or the number of features you use,
-        we make it easy to control your budget with clear, easy to understand billing.
-      </HighlightBox>
-      <Column>
-        <img src={PayImage} />
-      </Column>
-    </ResponsiveRow>
-    <ResponsiveRow style={{marginTop:50}}>
-      <InfoBox color="#3A3C3E" icon="payments" title="Status-based Payments">
-        Pay only for the time your reps are actively working on the system–nothing more.
-      </InfoBox>
-      <InfoBox color="#3A3C3E" icon="international" title="Pay Internationally">
-        With reps from all over the globe, payments can get tricky.
-        We handle international payments so you don’t have to.
-      </InfoBox>
-      <InfoBox color="#3A3C3E" icon="billing" title="Integrated Billing">
-        We facilitate payments on your behalf and provide you with comprehensive invoices.
-      </InfoBox>
-    </ResponsiveRow>
+    <Container>
+      <ResponsiveRow>
+        <HighlightBox title="Pay" position="left">
+          No matter the size of your team or the number of features you use,
+          we make it easy to control your budget with clear, easy to understand billing.
+        </HighlightBox>
+        <Column>
+          <img src={PayImage} />
+        </Column>
+      </ResponsiveRow>
+      <ResponsiveRow style={{marginTop:50}}>
+        <InfoBox color="#3A3C3E" icon="payments" title="Status-based Payments">
+          Pay only for the time your reps are actively working on the system–nothing more.
+        </InfoBox>
+        <InfoBox color="#3A3C3E" icon="international" title="Pay Internationally">
+          With reps from all over the globe, payments can get tricky.
+          We handle international payments so you don’t have to.
+        </InfoBox>
+        <InfoBox color="#3A3C3E" icon="billing" title="Integrated Billing">
+          We facilitate payments on your behalf and provide you with comprehensive invoices.
+        </InfoBox>
+      </ResponsiveRow>
+    </Container>
   </PaySectionWrap>
 );
 
