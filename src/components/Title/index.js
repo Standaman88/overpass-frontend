2d77c25c5627e4d49790c2c30608e28c6d9dc049
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 21:19:05 -0400

    bugfix src/components/Title/index.js

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-export const Title = styled.h3`
+const Title = styled.h3`
   font-family: Arial, san-serif;
   font-size: 42px;
   font-weigth: bold;
   margin: 0;
   margin-bottom: 40px;
 `;
+
+export default Title;
