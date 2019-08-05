39d462f6aea746200897936a5a486e6846fca23e
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-04 20:54:52 -0400

    added HighlightBox and MonitorImage @ src/components/MonitorSection

--- src/components/MonitorSection/index.js
+++ src/components/MonitorSection/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import HighlightBox from '../HighlightBox'
 import InfoBox from '../InfoBox'
 import { Column, Row } from '../Layout'
 import Section from '../Section'
 import { MonitorSectionWrap } from './styles'
+import MonitorImage from './images/monitor.svg'
 
 
 const MonitorSection = () => (
   <MonitorSectionWrap>
-    Monitor Section
+    <Row>
+      <Column>
+        <img src={MonitorImage} />
+      </Column>
+      <HighlightBox title="Monitor" position="right">
+        Gain real time insights into your reps performance get total
+        visibility into every interaction with your customers.
+      </HighlightBox>
+    </Row>
     <Row>
       <InfoBox color="#3A3C3E" icon="outcomes" title="Call Outcomes">Easily view the outcomes of all calls and quickly evaulate your campaign success.</InfoBox>
       <InfoBox color="#3A3C3E" icon="recording" title="Recording">Calls are automatically recorded and saved, and can be listened to at any time.</InfoBox>
