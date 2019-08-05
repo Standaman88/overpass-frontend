8348c6e80320f2636398e2e6153fb31e0ea25e4b
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 03:02:14 -0400

    deleted:    src/components/IntroSection/images/hero.svg deleted:    src/components/IntroSection/index.js deleted:    src/components/IntroSection/styles.js

--- src/components/IntroSection/styles.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-import { SectionPrimary } from '../Section'
-import HeroImage from './images/hero.svg'
-console.log("HeroImage", HeroImage)
-
-export const IntroSectionWrap = styled(SectionPrimary)`
-  background-image: url(${HeroImage})
-`;
