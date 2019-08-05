44d59831511f8069f8fdb3fa2f5b423d1a5f4641
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 01:20:24 -0400

    added Container @ src/components/PricingSection/index.js

--- src/components/PricingSection/index.js
+++ src/components/PricingSection/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Column, Row, ResponsiveRow } from '../Layout'
+import Container from '../Container'
 import Paragraph from '../Paragraph'
 import Price from '../Price'
 import Box from '../Box'
@@ -15,28 +16,30 @@ const PricingParagraph = styled(Paragraph)`
 
 const PricingSection = () => (
   <PricingSectionWrap>
-    <Column>
-      <Title as="h3">Transparent Pricing</Title>
-      <PricingParagraph color="#3A3C3E" size="large" style={{maxWidth:650}}>
-        We’ve made our pricing as simple as possible.
-        With our free call center software, all you pay for is
-        talk-time and your contractor’s hourly rate.
-      </PricingParagraph>
-      <ResponsiveRow style={{justifyContent:'center'}}>
-        <Box title="Average Marketplace Rates">
-          <Row>
-            <Price before="$">5- </Price>
-            <Price before="$" time="/hour">20</Price>
-          </Row>
-        </Box>
+    <Container>
+      <Column>
+        <Title as="h3">Transparent Pricing</Title>
+        <PricingParagraph color="#3A3C3E" size="large" style={{maxWidth:650}}>
+          We’ve made our pricing as simple as possible.
+          With our free call center software, all you pay for is
+          talk-time and your contractor’s hourly rate.
+        </PricingParagraph>
+        <ResponsiveRow style={{justifyContent:'center'}}>
+          <Box title="Average Marketplace Rates">
+            <Row>
+              <Price before="$">5- </Price>
+              <Price before="$" time="/hour">20</Price>
+            </Row>
+          </Box>
 
-        <Box title="Make Standard Calls">
-          <Row>
-            <Price after="&#162;" time="/minute">2.9</Price>
-          </Row>
-        </Box>
-      </ResponsiveRow>
-    </Column>
+          <Box title="Make Standard Calls">
+            <Row>
+              <Price after="&#162;" time="/minute">2.9</Price>
+            </Row>
+          </Box>
+        </ResponsiveRow>
+      </Column>
+    </Container>
   </PricingSectionWrap>
 );
 
