e9100ac9f1169f29c56fc7aa846ad3522459f0c7
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 03:34:11 -0400

    finish src/components/NavbarSection

--- src/components/NavbarSection/index.js
+++ src/components/NavbarSection/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column, Row, ResponsiveRow, Half } from '../Layout'
+import Button from '../Button'
+import Container from '../Container'
+import Logo from '../Logo'
 import Section from '../Section'
-import { NavbarSectionWrap } from './styles'
+import { NavbarSectionWrap, NavButton } from './styles'
 
 
 const NavbarSection = () => (
   <NavbarSectionWrap>
-    Navbar Section
+    <Container>
+      <ResponsiveRow>
+        <Half style={{alignItems:'flex-start'}}>
+          <Logo name="overpass" />
+        </Half>
+        <Half style={{alignItems:'flex-end'}}>
+          <Row>
+            <NavButton border="none">Features</NavButton>
+            <NavButton border="none">Pricing</NavButton>
+            <NavButton xcolor="#1E95EE">Login</NavButton>
+          </Row>
+        </Half>
+      </ResponsiveRow>
+    </Container>
   </NavbarSectionWrap>
 );
 
--- src/components/NavbarSection/styles.js
+++ src/components/NavbarSection/styles.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from '../Button'
 import { SectionPrimary } from '../Section'
 
 export const NavbarSectionWrap = styled(SectionPrimary)`
   // styles go here
+`;
+
+export const NavButton = styled(Button)`
+  border-radius: 40px;
+  border-color: ${props => props.border || 'white'};
+  color: white;
+  background: #0092FF;
+  height: 40px;
+  padding: 0 30px;
+  min-width: auto;
+  width: auto;
 `;
