11225d8b1f469b716b324e3f32b37c347f2d7418
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 02:13:50 -0400

    added src/components/Logo/

---
+++ src/components/Logo/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+import LogoFlexport from './images/flexport.svg'
+import LogoHandy from './images/handy.svg'
+import LogoIntel from './images/intel.svg'
+import LogoJohnson from './images/johnson.svg'
+import LogoMonday from './images/monday.svg'
+
+const LOGOS = {
+  flexport: LogoFlexport,
+  handy: LogoHandy,
+  intel: LogoIntel,
+  johnson: LogoJohnson,
+  monday: LogoMonday,
+};
+
+export const Logo = styled.img.attrs(({ name="intel" }) => ({
+  src: name in LOGOS ? LOGOS[name] : name
+}))`
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  height: 50px;
+  width: auto;
+`;
+
+export default Logo;
