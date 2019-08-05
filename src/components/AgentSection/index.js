67858f887ac382da2d4d3f5f275b250f583efc73
Stanley Edmond <stanleyedmond4@gmail.com>
2019-08-05 00:46:04 -0400

    added image and elements @ src/components/AgentSection

--- src/components/AgentSection/index.js
+++ src/components/AgentSection/index.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column, Row, ResponsiveRow } from '../Layout'
+import Paragraph from '../Paragraph'
 import Section from '../Section'
-import { AgentSectionWrap } from './styles'
+import Title from '../Title'
+import SearchImage from './images/search.svg'
+import ProfilesImage from './images/profiles.svg'
+import { AgentSectionWrap, Half, Subtitle } from './styles'
 
 
 const AgentSection = () => (
   <AgentSectionWrap>
-    Agent Section
+    <ResponsiveRow>
+      <Half>
+        <img src={SearchImage} />
+        <Title as="h3">Find the perfect agent</Title>
+        <Paragraph color="#3A3C3E" size="large" style={{maxWidth:445,textAlign:'center'}}>
+          No more browsing through thousands of applications.
+          Each week, we’ll send you a fresh batch of hand-picked,
+          personally-vetted candidates.
+        </Paragraph>
+      </Half>
+      <Half>
+        <Subtitle as="h5">
+          Selections from the <br />Overpass Marketplace
+        </Subtitle>
+        <img src={ProfilesImage} />
+      </Half>
+    </ResponsiveRow>
   </AgentSectionWrap>
 );
 
--- src/components/AgentSection/styles.js
+++ src/components/AgentSection/styles.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Column, Row, ResponsiveRow } from '../Layout'
 import Section from '../Section'
+import Title from '../Title'
 
 export const AgentSectionWrap = styled(Section)`
   // styles go here
+`;
+
+export const Subtitle = styled(Title)`
+  line-height: 1.5em;
+  text-align: center;
+`;
+
+export const Half = styled(Column)`
+  flex-basis: 50%;
+  flex-grow: 1;
+  justify-content: center;
 `;
